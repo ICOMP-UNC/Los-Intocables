acf54749d617a87e20f81318daf76b0b56643985
EnzoVers <enzogvers@hotmail.com>
2024-11-13 19:53:17 -0300

    lib correcciones 2

--- lib/CMSISv2p00_LPC17xx/drivers/src/lpc17xx_mcpwm.c
+++ lib/CMSISv2p00_LPC17xx/drivers/src/lpc17xx_mcpwm.c
@@ -804,12 +804,12 @@ void MCPWM_ACMode(LPC_MCPWM_TypeDef *MCPWMx, uint32_t acMode) {
   MCOA outputs.
   * 								- DISABLE: The
   MCOB outputs have the same basic
-  * 									polarity as
-  the MCOA outputs.
+  * 									polarity
+  as the MCOA outputs.
   * @param[in]	outputPattern	A value contains bits that enables/disables the
   specified
-  * 								output pins route
-  to the internal MCOA0 signal, should be:
+  * 								output pins
+  route to the internal MCOA0 signal, should be:
                                  - MCPWM_PATENT_A0: 	 MCOA0 tracks internal
   MCOA0
                                  - MCPWM_PATENT_B0: 	 MCOB0 tracks internal
