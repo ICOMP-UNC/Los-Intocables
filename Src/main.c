a20230463065a62b327a27a314d4450dc3ae561c
LisandroTesta1 <lisandro.testa@mi.unc.edu.ar>
2024-11-15 21:52:33 -0300

    format2

--- Src/main.c
+++ Src/main.c
@@ -26,39 +26,38 @@
 /** @defgroup Pines Pines asociados a periféricos
  * @{
  */
-#define PIN_ADC0_CH0 ((uint32_t)(1 << 23))     /**< P0.23 - Canal 0 del ADC */
-#define PIN_ADC0_CH1 ((uint32_t)(1 << 24))     /**< P0.24 - Canal 1 del ADC */
-#define PIN_ADC0_CH2 ((uint32_t)(1 << 25))     /**< P2.10 - Canal 2 del ADC */
-#define PIN_PWM1 ((uint32_t)(1 << 18))         /**< P1.18 - PWM */
-#define PIN_DAC ((uint32_t)(1 << 26))          /**< P0.26 - DAC */
-#define PIN_LED_VERDE ((uint32_t)(1 << 4))     /**< P0.04 - LED verde */
-#define PIN_LED_ROJO ((uint32_t)(1 << 5))      /**< P0.05 - LED rojo */
-#define PIN_LED_UART ((uint32_t)(1 << 6))      /**< P0.06 - LED UART */
+#define PIN_ADC0_CH0     ((uint32_t)(1 << 23)) /**< P0.23 - Canal 0 del ADC */
+#define PIN_ADC0_CH1     ((uint32_t)(1 << 24)) /**< P0.24 - Canal 1 del ADC */
+#define PIN_ADC0_CH2     ((uint32_t)(1 << 25)) /**< P2.10 - Canal 2 del ADC */
+#define PIN_PWM1         ((uint32_t)(1 << 18)) /**< P1.18 - PWM */
+#define PIN_DAC          ((uint32_t)(1 << 26)) /**< P0.26 - DAC */
+#define PIN_LED_VERDE    ((uint32_t)(1 << 4))  /**< P0.04 - LED verde */
+#define PIN_LED_ROJO     ((uint32_t)(1 << 5))  /**< P0.05 - LED rojo */
+#define PIN_LED_UART     ((uint32_t)(1 << 6))  /**< P0.06 - LED UART */
 #define PIN_BOTON_PUERTA ((uint32_t)(1 << 13)) /**< P2.13 - Botón de puerta */
-#define PIN_SALIDA_UART ((uint32_t)(1 << 2))   /**< P0.02 - Salida UART */
-#define PIN_DIR_MPAP                                                           \
-  ((uint32_t)(1 << 7)) /**< P0.07 - Dirección motor paso a paso */
+#define PIN_SALIDA_UART  ((uint32_t)(1 << 2))  /**< P0.02 - Salida UART */
+#define PIN_DIR_MPAP     ((uint32_t)(1 << 7))  /**< P0.07 - Dirección motor paso a paso */
 /** @} */
 
 /** @defgroup Valores de configuración de tiempos
  * @{
  */
-#define VALOR_PRESCALER 100    /**< Prescaler para temporizador - 100 us */
-#define MATCH0_TIM0 10000      /**< Match0 - 1 segundo */
-#define VAL_SYSTICK 100        /**< Valor de SYSTICK - 100 ms */
-#define VAL_TIEMPO_DAC 250000  /**< Tiempo de salida del DAC - 10 ms */
-#define VAL_PRESCALER_PWM 1    /**< Prescaler del PWM - 1 us */
-#define VAL_PERIODO_PWM 2000   /**< Periodo del PWM - 2 ms */
-#define VAL_DUTYCICLE_PWM 1000 /**< Duty cycle del PWM - 1 ms */
+#define VALOR_PRESCALER   100    /**< Prescaler para temporizador - 100 us */
+#define MATCH0_TIM0       10000  /**< Match0 - 1 segundo */
+#define VAL_SYSTICK       100    /**< Valor de SYSTICK - 100 ms */
+#define VAL_TIEMPO_DAC    250000 /**< Tiempo de salida del DAC - 10 ms */
+#define VAL_PRESCALER_PWM 1      /**< Prescaler del PWM - 1 us */
+#define VAL_PERIODO_PWM   2000   /**< Periodo del PWM - 2 ms */
+#define VAL_DUTYCICLE_PWM 1000   /**< Duty cycle del PWM - 1 ms */
 /** @} */
 
 /** @defgroup Estados del sistema
  * @{
  */
-#define ON 1       /**< Encendido */
-#define OFF 0      /**< Apagado */
-#define ABRIR 1    /**< Abrir */
-#define CERRAR 0   /**< Cerrar */
+#define ON       1 /**< Encendido */
+#define OFF      0 /**< Apagado */
+#define ABRIR    1 /**< Abrir */
+#define CERRAR   0 /**< Cerrar */
 #define PWM1_MR0 0 /**< Canal PWM 0 */
 #define PWM1_MR1 1 /**< Canal PWM 1 */
 /** @} */
@@ -66,17 +65,16 @@
 /** @defgroup Frecuencias del sistema
  * @{
  */
-#define FREQ_ADC 100000   /**< Frecuencia del ADC - 100 kHz */
-#define UART_BAUDIOS 9600 /**< Frecuencia UART - 9600 bps */
+#define FREQ_ADC     100000 /**< Frecuencia del ADC - 100 kHz */
+#define UART_BAUDIOS 9600   /**< Frecuencia UART - 9600 bps */
 /** @} */
 
 /** @brief Variables globales del sistema */
-volatile uint8_t Datos[4]; /**< Buffer de datos del sistema (puerta,
-                              temperatura, luz, gas) */
-volatile uint16_t
-    Conversiones[3];             /**< Buffer para valores convertidos por ADC */
-volatile uint16_t Valor_DAC = 0; /**< Valor actual del DAC */
-volatile uint8_t Count_PWM = 0;  /**< Contador para PWM */
+volatile uint8_t Datos[4];         /**< Buffer de datos del sistema (puerta,
+                                      temperatura, luz, gas) */
+volatile uint16_t Conversiones[3]; /**< Buffer para valores convertidos por ADC */
+volatile uint16_t Valor_DAC = 0;   /**< Valor actual del DAC */
+volatile uint8_t Count_PWM = 0;    /**< Contador para PWM */
 
 /** @brief Prototipos de funciones */
 void ToggleStatusDoor(void);
@@ -103,23 +101,25 @@ void Config_GPDMA(void);
  *
  * @return 0 Si se ejecuta correctamente.
  */
-int main(void) {
-  SystemInit(); /**< Inicializa el sistema */
+int main(void)
+{
+    SystemInit(); /**< Inicializa el sistema */
 
-  Config_GPIO();
-  Config_SYSTICK();
-  Config_TIMER0();
-  Config_ADC();
-  Config_DAC();
-  Config_PWM();
-  Config_UART();
-  Config_GPDMA();
+    Config_GPIO();
+    Config_SYSTICK();
+    Config_TIMER0();
+    Config_ADC();
+    Config_DAC();
+    Config_PWM();
+    Config_UART();
+    Config_GPDMA();
 
-  while (TRUE) {
-    /* Ciclo principal */
-  }
+    while (TRUE)
+    {
+        /* Ciclo principal */
+    }
 
-  return 0;
+    return 0;
 }
 
 /**
@@ -128,44 +128,45 @@ int main(void) {
  * Esta función inicializa el DMA del LPC1769, configurando los canales
  * y estructuras necesarias para la transferencia de datos entre periféricos.
  */
-void Config_GPDMA(void) {
-  GPDMA_Channel_CFG_Type ChannelCfg0; /**< Configuración para el canal 0 */
-  GPDMA_Channel_CFG_Type ChannelCfg1; /**< Configuración para el canal 1 */
+void Config_GPDMA(void)
+{
+    GPDMA_Channel_CFG_Type ChannelCfg0; /**< Configuración para el canal 0 */
+    GPDMA_Channel_CFG_Type ChannelCfg1; /**< Configuración para el canal 1 */
 
-  GPDMA_LLI_Type ListADC; /**< Configuración de lista vinculada para el ADC */
+    GPDMA_LLI_Type ListADC; /**< Configuración de lista vinculada para el ADC */
 
-  // Configuración de la Lista de Transferencias
-  ListADC.DstAddr = (uint16_t)&Conversiones[0];
-  ListADC.SrcAddr = GPDMA_CONN_ADC;
-  ListADC.NextLLI = 0;
-  ListADC.Control = (3 << 0) | (1 << 18) | (1 << 21) | (1 << 27);
+    // Configuración de la Lista de Transferencias
+    ListADC.DstAddr = (uint16_t)&Conversiones[0];
+    ListADC.SrcAddr = GPDMA_CONN_ADC;
+    ListADC.NextLLI = 0;
+    ListADC.Control = (3 << 0) | (1 << 18) | (1 << 21) | (1 << 27);
 
-  // Inicialización del GPDMA
-  GPDMA_Init();
+    // Inicialización del GPDMA
+    GPDMA_Init();
 
-  // Configuración del Canal 0 (Periférico a memoria)
-  ChannelCfg0.ChannelNum = 0;
-  ChannelCfg0.TransferType = GPDMA_TRANSFERTYPE_P2M;
-  ChannelCfg0.TransferSize = 0;
-  ChannelCfg0.SrcConn = GPDMA_CONN_ADC;
-  ChannelCfg0.DstMemAddr = (uint32_t)&Conversiones[0];
-  ChannelCfg0.DMALLI = (uint32_t)&ListADC;
+    // Configuración del Canal 0 (Periférico a memoria)
+    ChannelCfg0.ChannelNum = 0;
+    ChannelCfg0.TransferType = GPDMA_TRANSFERTYPE_P2M;
+    ChannelCfg0.TransferSize = 0;
+    ChannelCfg0.SrcConn = GPDMA_CONN_ADC;
+    ChannelCfg0.DstMemAddr = (uint32_t)&Conversiones[0];
+    ChannelCfg0.DMALLI = (uint32_t)&ListADC;
 
-  // Configuración del Canal 1 (Memoria a periférico)
-  ChannelCfg1.ChannelNum = 1;
-  ChannelCfg1.TransferType = GPDMA_TRANSFERTYPE_M2P;
-  ChannelCfg1.TransferSize = 1;
-  ChannelCfg1.SrcMemAddr = (uint32_t)&Valor_DAC;
-  ChannelCfg1.DstConn = GPDMA_CONN_DAC;
-  ChannelCfg1.DMALLI = 0;
+    // Configuración del Canal 1 (Memoria a periférico)
+    ChannelCfg1.ChannelNum = 1;
+    ChannelCfg1.TransferType = GPDMA_TRANSFERTYPE_M2P;
+    ChannelCfg1.TransferSize = 1;
+    ChannelCfg1.SrcMemAddr = (uint32_t)&Valor_DAC;
+    ChannelCfg1.DstConn = GPDMA_CONN_DAC;
+    ChannelCfg1.DMALLI = 0;
 
-  // Configuración de los canales
-  GPDMA_Setup(&ChannelCfg0);
-  GPDMA_Setup(&ChannelCfg1);
+    // Configuración de los canales
+    GPDMA_Setup(&ChannelCfg0);
+    GPDMA_Setup(&ChannelCfg1);
 
-  // Habilitación de los canales
-  GPDMA_ChannelCmd(0, ENABLE);
-  GPDMA_ChannelCmd(1, ENABLE);
+    // Habilitación de los canales
+    GPDMA_ChannelCmd(0, ENABLE);
+    GPDMA_ChannelCmd(1, ENABLE);
 }
 
 /**
@@ -174,20 +175,19 @@ void Config_GPDMA(void) {
  * Esta función inicializa y habilita los canales del ADC para la adquisición de
  * datos analógicos, además de habilitar las interrupciones correspondientes.
  */
-void Config_ADC(void) {
-  ADC_Init(
-      LPC_ADC,
-      FREQ_ADC); /**< Inicialización del ADC con una frecuencia especificada */
+void Config_ADC(void)
+{
+    ADC_Init(LPC_ADC, FREQ_ADC); /**< Inicialización del ADC con una frecuencia especificada */
 
-  // Habilitar canales ADC
-  ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
-  ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_1, ENABLE);
-  ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_2, ENABLE);
+    // Habilitar canales ADC
+    ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
+    ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_1, ENABLE);
+    ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_2, ENABLE);
 
-  // Configurar interrupciones de los canales
-  ADC_IntConfig(LPC_ADC, ADC_CHANNEL_0, ENABLE);
-  ADC_IntConfig(LPC_ADC, ADC_CHANNEL_1, ENABLE);
-  ADC_IntConfig(LPC_ADC, ADC_CHANNEL_2, ENABLE);
+    // Configurar interrupciones de los canales
+    ADC_IntConfig(LPC_ADC, ADC_CHANNEL_0, ENABLE);
+    ADC_IntConfig(LPC_ADC, ADC_CHANNEL_1, ENABLE);
+    ADC_IntConfig(LPC_ADC, ADC_CHANNEL_2, ENABLE);
 }
 
 /**
@@ -196,29 +196,30 @@ void Config_ADC(void) {
  * Inicializa la UART con los parámetros de configuración especificados,
  * habilitando el modo FIFO y las interrupciones para transmisión.
  */
-void Config_UART(void) {
-  UART_CFG_Type uart; /**< Estructura para configuración de UART */
+void Config_UART(void)
+{
+    UART_CFG_Type uart; /**< Estructura para configuración de UART */
 
-  // Configuración de parámetros básicos
-  uart.Baud_rate = UART_BAUDIOS;
-  uart.Databits = UART_DATABIT_8;
-  uart.Parity = UART_PARITY_NONE;
-  uart.Stopbits = UART_STOPBIT_1;
+    // Configuración de parámetros básicos
+    uart.Baud_rate = UART_BAUDIOS;
+    uart.Databits = UART_DATABIT_8;
+    uart.Parity = UART_PARITY_NONE;
+    uart.Stopbits = UART_STOPBIT_1;
 
-  UART_Init(LPC_UART2, &uart); /**< Inicialización de UART2 */
+    UART_Init(LPC_UART2, &uart); /**< Inicialización de UART2 */
 
-  UART_FIFO_CFG_Type fifo; /**< Configuración del FIFO */
+    UART_FIFO_CFG_Type fifo; /**< Configuración del FIFO */
 
-  // Configuración del FIFO
-  fifo.FIFO_DMAMode = ENABLE;
-  fifo.FIFO_Level = UART_FIFO_TRGLEV2;
-  fifo.FIFO_ResetTxBuf = ENABLE;
+    // Configuración del FIFO
+    fifo.FIFO_DMAMode = ENABLE;
+    fifo.FIFO_Level = UART_FIFO_TRGLEV2;
+    fifo.FIFO_ResetTxBuf = ENABLE;
 
-  UART_FIFOConfig(LPC_UART2, &fifo);
+    UART_FIFOConfig(LPC_UART2, &fifo);
 
-  // Habilitar transmisión e interrupciones
-  UART_TxCmd(LPC_UART2, ENABLE);
-  UART_IntConfig(LPC_UART2, UART_INTCFG_THRE, ENABLE);
+    // Habilitar transmisión e interrupciones
+    UART_TxCmd(LPC_UART2, ENABLE);
+    UART_IntConfig(LPC_UART2, UART_INTCFG_THRE, ENABLE);
 }
 
 /**
@@ -227,40 +228,40 @@ void Config_UART(void) {
  * Configura el PWM para generar señales con un duty cycle y periodo específico,
  * utilizando un único canal en modo de borde simple.
  */
-void Config_PWM(void) {
-  PWM_TIMERCFG_Type PWMCfg; /**< Configuración del temporizador PWM */
-  PWM_MATCHCFG_Type
-      match0; /**< Configuración del registro de coincidencia PWM */
-  PINSEL_CFG_Type PinCgf; /**< Configuración de los pines PWM */
+void Config_PWM(void)
+{
+    PWM_TIMERCFG_Type PWMCfg; /**< Configuración del temporizador PWM */
+    PWM_MATCHCFG_Type match0; /**< Configuración del registro de coincidencia PWM */
+    PINSEL_CFG_Type PinCgf;   /**< Configuración de los pines PWM */
 
-  // Configuración del pin PWM
-  PinCgf.Portnum = PINSEL_PORT_1;
-  PinCgf.Pinnum = PINSEL_PIN_18;
-  PinCgf.Funcnum = PINSEL_FUNC_2;
-  PinCgf.Pinmode = PINSEL_PINMODE_PULLDOWN;
-  PinCgf.OpenDrain = PINSEL_PINMODE_NORMAL;
+    // Configuración del pin PWM
+    PinCgf.Portnum = PINSEL_PORT_1;
+    PinCgf.Pinnum = PINSEL_PIN_18;
+    PinCgf.Funcnum = PINSEL_FUNC_2;
+    PinCgf.Pinmode = PINSEL_PINMODE_PULLDOWN;
+    PinCgf.OpenDrain = PINSEL_PINMODE_NORMAL;
 
-  PINSEL_ConfigPin(&PinCgf);
+    PINSEL_ConfigPin(&PinCgf);
 
-  // Configuración del temporizador PWM
-  PWMCfg.PrescaleOption = PWM_TIMER_PRESCALE_USVAL;
-  PWMCfg.PrescaleValue = VAL_PRESCALER_PWM;
+    // Configuración del temporizador PWM
+    PWMCfg.PrescaleOption = PWM_TIMER_PRESCALE_USVAL;
+    PWMCfg.PrescaleValue = VAL_PRESCALER_PWM;
 
-  PWM_Init(LPC_PWM1, PWM_MODE_TIMER, &PWMCfg);
+    PWM_Init(LPC_PWM1, PWM_MODE_TIMER, &PWMCfg);
 
-  // Configuración del periodo y duty cycle
-  PWM_MatchUpdate(LPC_PWM1, PWM1_MR0, VAL_PERIODO_PWM, PWM_MATCH_UPDATE_NOW);
-  PWM_MatchUpdate(LPC_PWM1, PWM1_MR1, VAL_DUTYCICLE_PWM, PWM_MATCH_UPDATE_NOW);
+    // Configuración del periodo y duty cycle
+    PWM_MatchUpdate(LPC_PWM1, PWM1_MR0, VAL_PERIODO_PWM, PWM_MATCH_UPDATE_NOW);
+    PWM_MatchUpdate(LPC_PWM1, PWM1_MR1, VAL_DUTYCICLE_PWM, PWM_MATCH_UPDATE_NOW);
 
-  match0.MatchChannel = 0;
-  match0.IntOnMatch = ENABLE;
-  match0.ResetOnMatch = ENABLE;
+    match0.MatchChannel = 0;
+    match0.IntOnMatch = ENABLE;
+    match0.ResetOnMatch = ENABLE;
 
-  PWM_ConfigMatch(LPC_PWM1, &match0);
+    PWM_ConfigMatch(LPC_PWM1, &match0);
 
-  // Configuración del canal y habilitación
-  PWM_ChannelConfig(LPC_PWM1, 1, PWM_CHANNEL_SINGLE_EDGE);
-  PWM_ChannelCmd(LPC_PWM1, 1, ENABLE);
+    // Configuración del canal y habilitación
+    PWM_ChannelConfig(LPC_PWM1, 1, PWM_CHANNEL_SINGLE_EDGE);
+    PWM_ChannelCmd(LPC_PWM1, 1, ENABLE);
 }
 
 /**
@@ -268,15 +269,17 @@ void Config_PWM(void) {
  *
  * Este manejador desactiva el PWM después de 100 pulsos.
  */
-void PWM1_IRQHandler(void) {
-  Count_PWM++;
+void PWM1_IRQHandler(void)
+{
+    Count_PWM++;
 
-  if (Count_PWM == 100) {
-    PWM_Cmd(LPC_PWM1, DISABLE);
-    Count_PWM = 0;
-  }
+    if (Count_PWM == 100)
+    {
+        PWM_Cmd(LPC_PWM1, DISABLE);
+        Count_PWM = 0;
+    }
 
-  PWM_ClearIntPending(LPC_PWM1, PWM_INTSTAT_MR0);
+    PWM_ClearIntPending(LPC_PWM1, PWM_INTSTAT_MR0);
 }
 
 /**
@@ -284,7 +287,8 @@ void PWM1_IRQHandler(void) {
  *
  * Envía los datos a través de UART cada vez que se genera una interrupción.
  */
-void Systick_Handler(void) {
-  UART_Send(LPC_UART0, &Datos, 4, NONE_BLOCKING);
-  SYSTICK_ClearCounterFlag();
+void Systick_Handler(void)
+{
+    UART_Send(LPC_UART0, &Datos, 4, NONE_BLOCKING);
+    SYSTICK_ClearCounterFlag();
 }
