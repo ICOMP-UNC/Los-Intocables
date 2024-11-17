92dd6ac4af59c0228c4d5cac910475115d0d693d
Enzo Verstraete <158090803+CGR-X@users.noreply.github.com>
2024-11-17 19:51:53 -0300

    Delete html directory

--- html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pwm1_5firqhandler_0',['PWM1_IRQHandler',['../main_8c.html#a723f02b408796deff17d7d759ac68819',1,'main.c']]]
-];
--- html/search/functions_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['uart2_5firqhandler_0',['UART2_IRQHandler',['../main_8c.html#ac20eca44aeea90e6f603831193cc9b28',1,'main.c']]]
-];
