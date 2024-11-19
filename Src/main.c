007fd298ab647eabe68c1df78d92ecee7de03bdf
EnzoVers <enzogvers@hotmail.com>
2024-11-19 18:04:44 -0300

    Update archive main.c - doxygen correction 3

--- Src/main.c
+++ Src/main.c
@@ -3,7 +3,7 @@
  * @brief
  * @authors Verstraete, Enzo - Campos, Mariano - Testa, Lisandro - Madrid, Santiago
  * @date 2024-11-17
- * \image html Diagrama_ED3_TPF.drawio.png "Diagrama de flujo de todo el sistema(main)"
+ * \image html /Diagrama_ED3_TPF.drawio.png "Diagrama de flujo de todo el sistema(main)"
  */
 
 #ifdef __USE_CMSIS
